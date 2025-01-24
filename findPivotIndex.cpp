5532ad52111b59247de6c698b0efd7e0f2c161a8
Aman Kumar <155160700+akdrin@users.noreply.github.com>
2025-01-24 10:13:53 +0530

    Create findPivotIndex.cpp

---
+++ findPivotIndex.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int totalSum=0; int leftSum=0;
+        for(int i=0;i<n;i++){
+            totalSum+=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            if(leftSum==(totalSum-leftSum-nums[i])) return i;
+            leftSum+=nums[i];
+        }
+        return -1;
+    }
+};
