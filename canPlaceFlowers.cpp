87279937445a84489c320b6d891d594514f8658a
Aman Kumar <155160700+akdrin@users.noreply.github.com>
2025-02-09 22:39:30 +0530

    Create canPlaceFlowers.cpp

---
+++ canPlaceFlowers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int m = flowerbed.size();   
+        for (int i = 0; i < m; i++) {
+            if (flowerbed[i] == 0) {
+                bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+                bool rightEmpty = (i == m - 1) || (flowerbed[i + 1] == 0);
+                
+                if (leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1; 
+                    n--;
+                    if (n == 0) return true;
+                    
+                    i++;
+                }
+            }
+        }
+        
+        return n <= 0;
+    }
+};
